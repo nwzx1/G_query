a92e97aef6e688fd62b6a5861a9c3350dd26a6e1
nawas charoliya <nawascharoliya1@gmail.com>
2023-09-25 16:35:11 +0000

    new

---
+++ action.java
@@ -0,0 +1,15 @@
+public class action {
+    public static int add(int num1 ,int num2) {
+        int ans = num1 + num2;
+        return ans;
+    }
+    public static int sub(int num1,int num2) {
+        return num1 - num2;
+    }
+    public static int div(int num1,int num2) {
+        return num1 / num2;
+    }
+    public static int mul(int num1,int num2) {
+        return num1 * num2;
+    }
+}
---
+++ main.java
@@ -0,0 +1,9 @@
+
+class main {
+
+    public static void main(String[] args) {
+        action math = new action();
+        int ans = math.add(1,2);
+        System.out.println(ans);
+    }
+}
